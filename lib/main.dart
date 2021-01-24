9500c510b3df5cdccedf2f59bba023e5b2c5f0f6
turkaytunc <turkaytunc00@gmail.com>
2021-01-24 11:47:54 +0300

    add: routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_timeapp/pages/home.dart';
+import 'package:flutter_timeapp/pages/loading.dart';
+import 'package:flutter_timeapp/pages/time.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Home(),
+    initialRoute: '/home',
+    routes: {
+      '/': (context) => Loading(),
+      '/home': (context) => Home(),
+      '/location': (context) => Time()
+    },
   ));
 }
 
