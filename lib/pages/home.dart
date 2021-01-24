684e7cdf112ceac32b338dfa95b4ffc020af8de9
turkaytunc <turkaytunc00@gmail.com>
2021-01-24 11:38:00 +0300

    add: home and loading screen

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -9,7 +9,9 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Home Screen'),
+      body: SafeArea(
+        child: Text('Home Screen'),
+      ),
     );
   }
 }
--- lib/pages/time.dart
+++ lib/pages/time.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class Time extends StatefulWidget {
+  @override
+  _TimeState createState() => _TimeState();
+}
+
+class _TimeState extends State<Time> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Text('Choose Location'),);
+  }
+}
